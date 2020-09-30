71dd9e170cc8913b655b18431de0bf4518f9c46e
parksanguk <psu@mz.co.kr>
2020-09-30 22:18:33 +0900

    update

--- backend-app/scripts/20_insert_sample_data.sql
+++ backend-app/scripts/20_insert_sample_data.sql
@@ -1,39 +1,39 @@
 -- REGION
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('北海道', current_timestamp);
+VALUES ('홋카이도', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('東北', current_timestamp);
+VALUES ('도호쿠', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('関東', current_timestamp);
+VALUES ('칸토', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('中部', current_timestamp);
+VALUES ('추부', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('近畿', current_timestamp);
+VALUES ('긴키', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('中国', current_timestamp);
+VALUES ('추고쿠', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('四国', current_timestamp);
+VALUES ('시코쿠', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('九州', current_timestamp);
+VALUES ('큐슈', current_timestamp);
 
 INSERT INTO region (region_name, creation_timestamp)
-VALUES ('沖縄', current_timestamp);
+VALUES ('오키나와', current_timestamp);
 
 -- LOCATION
 INSERT INTO location (location_name, region_id, note)
-VALUES ('美ら海水族館', (SELECT region_id FROM region WHERE region_name = '沖縄'),
-  '沖縄の代表的な水族館で、ジンベエザメをはじめ、様々な沖縄の海の生き物を見ることができます。');
+VALUES ('추라우미 수족관', (SELECT region_id FROM region WHERE region_name = '오키나와'),
+  '오키나와의 대표적인 수족관으로 고래 상어를 비롯하여 다양한 오키나와의 바다 생물을 볼 수 있다.');
 
 INSERT INTO location (location_name, region_id, note)
-VALUES ('首里城', (SELECT region_id FROM region WHERE region_name = '沖縄'),
-  '琉球王朝の王城で、世界遺産の1つです。');
+VALUES ('슈리성', (SELECT region_id FROM region WHERE region_name = '오키마와'),
+  '류큐 왕조의 왕성으로 세계유산 중 하나이다.');
 
 -- BATCH_PROCESSING
 INSERT INTO batch_processing (batch_name)
